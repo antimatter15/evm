e0e277fea5af57ac40e80a6808badf58c9ca1c50
Kevin Kwok <antimatter15@gmail.com>
2015-11-22 23:52:05 -0500

    reiir

--- brictor/multi.js
+++ brictor/multi.js
@@ -5,7 +5,7 @@ var margin = { top: 20, right: 20, bottom: 30, left: 50 },
 
 var svg = d3.select("body").append("svg")
     .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
+    .attr("height", 500 + height + margin.top + margin.bottom)
     .append("g")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
